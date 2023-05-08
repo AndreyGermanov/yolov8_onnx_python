6f9494ca765f8d5c01e4c73d149cc8c294ac25ea
Andrey Germanov <andrey@germanov.dev>
2023-05-08 15:11:47 +0300

    Implementation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# yolov8_onnx_python
-YOLOv8 inference using Julia
+# YOLOv8 inference using Python
+
+This is a web interface to [YOLOv8 object detection neural network](https://ultralytics.com/yolov8) 
+implemented on [Python](https://www.python.org) via [ONNX Runtime](https://onnxruntime.ai/docs/get-started/with-python.html).
+
+## Install
+
+* Clone this repository: `git clone git@github.com:AndreyGermanov/yolov8_onnx_python.git`
+* Go to the root of cloned repository
+* Install dependencies by running `pip3 install -r requirements.txt`
+
+## Run
+
+Execute:
+
+```
+python3 object_detector.py
+```
+
+It will start a webserver on http://localhost:8080. Use any web browser to open the web interface.
+
+Using the interface you can upload the image to the object detector and see bounding boxes of all  objects detected on it.
---
+++ object_detector.py
@@ -0,0 +1,173 @@
+import onnxruntime as ort
+from flask import request, Response, Flask
+from waitress import serve
+from PIL import Image
+import numpy as np
+import json
+
+app = Flask(__name__)
+
+
+def main():
+    serve(app, host='0.0.0.0', port=8080)
+
+
+@app.route("/")
+def root():
+    """
+    Site main page handler function.
+    :return: Content of index.html file
+    """
+    with open("index.html") as file:
+        return file.read()
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    """
+        Handler of /detect POST endpoint
+        Receives uploaded file with a name "image_file", passes it
+        through YOLOv8 object detection network and returns and array
+        of bounding boxes.
+        :return: a JSON array of objects bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    buf = request.files["image_file"]
+    boxes = detect_objects_on_image(buf.stream)
+    return Response(json.dumps(boxes),  mimetype='application/json')
+
+
+def detect_objects_on_image(buf):
+    """
+    Function receives an image,
+    passes it through YOLOv8 neural network
+    and returns an array of detected objects
+    and their bounding boxes
+    :param buf: Input image file stream
+    :return: Array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    input, img_width, img_height = prepare_input(buf)
+    output = run_model(input)
+    return process_output(output, img_width, img_height)
+
+
+def prepare_input(buf):
+    """
+    Function used to convert input image to tensor,
+    required as an input to YOLOv8 object detection
+    network.
+    :param buf: Uploaded file input stream
+    :return: Numpy array in a shape (3,width,height) where 3 is number of color channels
+    """
+    img = Image.open(buf)
+    img_width, img_height = img.size
+    img = img.resize((640, 640))
+    img = img.convert("RGB")
+    input = np.array(img) / 255.0
+    input = input.transpose(2, 0, 1)
+    input = input.reshape(1, 3, 640, 640)
+    return input.astype(np.float32), img_width, img_height
+
+
+def run_model(input):
+    """
+    Function used to pass provided input tensor to
+    YOLOv8 neural network and return result
+    :param input: Numpy array in a shape (3,width,height)
+    :return: Raw output of YOLOv8 network as an array of shape (1,84,8400)
+    """
+    model = ort.InferenceSession("yolov8m.onnx")
+    outputs = model.run(["output0"], {"images":input})
+    return outputs[0]
+
+
+def process_output(output, img_width, img_height):
+    """
+    Function used to convert RAW output from YOLOv8 to an array
+    of detected objects. Each object contain the bounding box of
+    this object, the type of object and the probability
+    :param output: Raw output of YOLOv8 network which is an array of shape (1,84,8400)
+    :param img_width: The width of original image
+    :param img_height: The height of original image
+    :return: Array of detected objects in a format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    output = output[0].astype(float)
+    output = output.transpose()
+
+    boxes = []
+    for row in output:
+        prob = row[4:].max()
+        if prob < 0.5:
+            continue
+        class_id = row[4:].argmax()
+        label = yolo_classes[class_id]
+        xc, yc, w, h = row[:4]
+        x1 = (xc - w/2) / 640 * img_width
+        y1 = (yc - h/2) / 640 * img_height
+        x2 = (xc + w/2) / 640 * img_width
+        y2 = (yc + h/2) / 640 * img_height
+        boxes.append([x1, y1, x2, y2, label, prob])
+
+    boxes.sort(key=lambda x: x[5], reverse=True)
+    result = []
+    while len(boxes) > 0:
+        result.append(boxes[0])
+        boxes = [box for box in boxes if iou(box, boxes[0]) < 0.5]
+
+    return result
+
+
+def iou(box1,box2):
+    """
+    Function calculates "Intersection-over-union" coefficient for specified two boxes
+    https://pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/.
+    :param box1: First box in format: [x1,y1,x2,y2,object_class,probability]
+    :param box2: Second box in format: [x1,y1,x2,y2,object_class,probability]
+    :return: Intersection over union ratio as a float number
+    """
+    return intersection(box1,box2)/union(box1,box2)
+
+
+def union(box1,box2):
+    """
+    Function calculates union area of two boxes
+    :param box1: First box in format [x1,y1,x2,y2,object_class,probability]
+    :param box2: Second box in format [x1,y1,x2,y2,object_class,probability]
+    :return: Area of the boxes union as a float number
+    """
+    box1_x1,box1_y1,box1_x2,box1_y2 = box1[:4]
+    box2_x1,box2_y1,box2_x2,box2_y2 = box2[:4]
+    box1_area = (box1_x2-box1_x1)*(box1_y2-box1_y1)
+    box2_area = (box2_x2-box2_x1)*(box2_y2-box2_y1)
+    return box1_area + box2_area - intersection(box1,box2)
+
+
+def intersection(box1,box2):
+    """
+    Function calculates intersection area of two boxes
+    :param box1: First box in format [x1,y1,x2,y2,object_class,probability]
+    :param box2: Second box in format [x1,y1,x2,y2,object_class,probability]
+    :return: Area of intersection of the boxes as a float number
+    """
+    box1_x1,box1_y1,box1_x2,box1_y2 = box1[:4]
+    box2_x1,box2_y1,box2_x2,box2_y2 = box2[:4]
+    x1 = max(box1_x1,box2_x1)
+    y1 = max(box1_y1,box2_y1)
+    x2 = min(box1_x2,box2_x2)
+    y2 = min(box1_y2,box2_y2)
+    return (x2-x1)*(y2-y1)
+
+
+# Array of YOLOv8 class labels
+yolo_classes = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+    "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
+    "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+    "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon",
+    "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut",
+    "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
+    "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+]
+
+main()
